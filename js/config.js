05c295569cf735f80c1b5cd5624cc3657b2ddb6a
Eric <eric.luong@outlook.com>
2012-12-21 14:33:17 +0100

    Add test function Eric in config.js

--- js/config.js
+++ js/config.js
@@ -2,3 +2,7 @@ $(document).on("mobileinit", function(){
 	$.mobile.page.prototype.options.backBtnText = ""; //enlever texte du bouton back
 	$.mobile.defaultPageTransition = 'slide'; //changer transition entre les pages
 });
+
+function Eric() {
+	return 'Eric';
+}
