135837cdc7d1f01e5eeedc70e10f3b97e8b1f338
Eric <eric.luong@outlook.com>
2013-01-22 22:43:18 +0100

    API : Add user id to informations

--- API/models/login.class.php
+++ API/models/login.class.php
@@ -11,7 +11,7 @@ class Login extends Tonic\Resource {
      */
     function getUser($mail,$password) {
 		$db = Database::getInstance();
-		$sql = "SELECT email, password FROM user WHERE email='".$mail."' AND password='".$password."'";
+		$sql = "SELECT id, email, password FROM user WHERE email='".$mail."' AND password='".$password."'";
 		$result = $db->fetch($sql);
 		if(empty($result[0])) return '';
 		return json_encode($result[0]);
