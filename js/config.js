834ea06556f20a1527a12361733362ec11bbebb9
Eric <eric.luong@outlook.com>
2013-01-29 16:56:04 +0100

    Little fixes (adaptation for mobile view)

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ $(document).on("mobileinit", function(){
 	$.mobile.allowCrossDomainPages = true;
 	$.support.cors = true;
 	$.mobile.page.prototype.options.backBtnText = ""; //enlever texte du bouton back
-	$.mobile.defaultPageTransition = 'slide'; //changer transition entre les pages
+	$.mobile.defaultPageTransition = 'none'; //changer transition entre les pages
 });
